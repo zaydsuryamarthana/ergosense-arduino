c69533478c05ffc48a89a2b61c77f6185290c5bd
Zayd Surya Marthana <zaydsuryamarthana@gmail.com>
2025-12-01 10:18:27 +0700

    upd-monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <LiquidCrystal_I2C.h>
 #include <NewPing.h>
 
 #define trigPin 9
@@ -7,9 +7,9 @@
 #define ledRed 2
 #define ledGreen 3
 #define caliBtn 5
-
 #define maxDistance 200
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 NewPing sonar(trigPin, echoPin, maxDistance);
 
 int idealDistance = 0;
@@ -20,66 +20,142 @@ const int threshold = 5000;
 
 void setup()
 {
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0,0);
+  lcd.print("   ERGOSENSE   ");
+  lcd.setCursor(0,1);
+  lcd.print("  ARDUINO UNO  ");
+
   pinMode(caliBtn, INPUT_PULLUP);
   pinMode(ledRed, OUTPUT);
   pinMode(ledGreen, OUTPUT);
-  Serial.begin(9600);
+
+  delay(3000);
+
+  calibrate();
+
 }
 
 void loop()
 {
   if (digitalRead(caliBtn) == LOW)
   {
+    delay(200);
     calibrate();
   }
 
   if (idealDistance == 0)
   {
-    digitalWrite(ledRed, HIGH);
-    delay(100);
-    digitalWrite(ledRed, LOW);
-    delay(100);
+    lcd.setCursor(0,0);
+    lcd.print("JARAK TERLALU");
+    lcd.setCursor(0,1);
+    lcd.print("DEKAT! TEKAN LAGI!");
+
+    return;
   }
 
   int currentDist = sonar.ping_cm();
   if (currentDist == 0)
     return;
 
+  lcd.setCursor(0,0);
+  lcd.print("SAFE ");
+  lcd.print(idealDistance);
+  lcd.print("cm | ");
+  lcd.print(currentDist);
+  lcd.print("cm");
+
   if (currentDist < (idealDistance - tolerance))
   {
-    if (badPosture == 0)
-    {
-      badPosture = millis();
-    }
-    if (millis() - badPosture > threshold)
-    {
-      trigger();
-    }
+    if (badPosture == 0) badPosture = millis();
+
+    lcd.setCursor(0,1);
+    lcd.print("ALERT! : ");
+    lcd.print((millis() - badPosture)/1000);
+    lcd.print("s ");
+    
+    if (millis() - badPosture > threshold) trigger();
   }
   else if (currentDist > (idealDistance - tolerance) && currentDist < (idealDistance + tolerance + 20))
   {
     reset();
+    lcd.setCursor(0,1);
+    lcd.print("KONDISI AMAN");
   }
   else
   {
-    reset();
+    if(badPosture == 0) badPosture = millis();
+
+    lcd.setCursor(0,1);
+    lcd.print("TERLALU JAUH!");
+    lcd.print((millis() - badPosture)/1000);
+    lcd.print("s ");
+
+    if(millis() - badPosture > threshold) {
+      trigger();
+    }
+
   }
   delay(100);
 }
 
 void calibrate()
 {
-  Serial.println("Tombol Ditekan! Memulai Kalibrasi...");
-  long total = 0;
-  for (int i = 0; i < 10; i++)
-  {
-    total += sonar.ping_cm();
-    delay(50);
-  }
-  idealDistance = total / 10;
-  Serial.print("Hasil Kalibrasi: ");
-  Serial.println(idealDistance);
-  digitalWrite(ledGreen, HIGH);
+  do {
+    digitalWrite(ledGreen, LOW);
+
+    lcd.clear();
+    lcd.backlight();
+
+    lcd.setCursor(0,0);
+    lcd.print("MULAI KALIBRASI!");
+    
+    for (int i=9; i>0; i--)
+    {
+      lcd.setCursor(0,1);
+      lcd.print("DALAM: ");
+      lcd.print(i);
+      lcd.print("s");
+
+      digitalWrite(ledRed, HIGH);
+      delay(200);
+      digitalWrite(ledRed, LOW);
+      delay(800);
+    }
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("SEDANG KALIBRASI!");
+
+    long total = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+      total += sonar.ping_cm();
+      lcd.setCursor(0,1);
+      lcd.print("JARAK : ");
+      lcd.print(total);
+      delay(50);
+    }
+
+    idealDistance = total / 10;
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("HASIL KALIBRASI");
+    lcd.setCursor(0,1);
+    lcd.print("JARAK : ");
+    lcd.print(idealDistance);
+    lcd.print(" cm");
+
+    digitalWrite(ledGreen, HIGH);
+
+    delay(3000);
+    lcd.clear();
+  }while (idealDistance <= tolerance);
+
 }
 
 void trigger()
